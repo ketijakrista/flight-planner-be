45b4c6090c090332b3b6e71c81b3565ddef4d2b8
Edgars Salmins <edgars.salmins@gmail.com>
2022-02-18 15:02:02 +0200

    cors update

--- FlightPlanner/Startup.cs
+++ FlightPlanner/Startup.cs
@@ -30,7 +30,12 @@ namespace FlightPlanner
 
             services.AddAuthentication("BasicAuthentication")
                 .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
-
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
